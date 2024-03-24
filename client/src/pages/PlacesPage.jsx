c00cba23a361d0be40b0ccfa6738b157df908471
arjun051 <arjunaggarwal05122004@gmail.com>
2024-03-24 10:12:36 +0530

    test commit

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -5,11 +5,11 @@ import axios from "axios";
 import PlaceImg from "../PlaceImg";
 export default function PlacesPage() {
   const [places,setPlaces] = useState([]);
-//   useEffect(() => {
-//     axios.get('/user-places').then(({data}) => {
-//       setPlaces(data);
-//     });
-//   }, []);
+  useEffect(() => {
+    axios.get('/user-places').then(({data}) => {
+      setPlaces(data);
+    });
+  }, []);
   return (
     <div>
       <AccountNav />
@@ -25,7 +25,9 @@ export default function PlacesPage() {
           {places.length > 0 && places.map(place => (
             <Link to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
               <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-                <PlaceImg place={place} />
+                {place.addedPhotos.length>0 && (
+                    <img src={place.addedPhotos[0]} alt="" />
+                )}
               </div>
               <div className="grow-0 shrink">
                 <h2 className="text-xl">{place.title}</h2>
